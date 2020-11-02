16a789ef9937f773946bac24ece933d14839dd8d
Nguyen Le <69196079+lecnguyenn@users.noreply.github.com>
2020-11-02 10:39:36 +0700

    Create Sort_array.m

---
+++ ThucHanh/Sort_array.m
@@ -0,0 +1,16 @@
+function y = Sort_array(x)
+n = length(x);
+if n<2
+    y=x;
+    return;
+end
+for i=1:n-1
+    if x(i) < x(i+1)
+        temp = x(i);
+        x(i) = x(i+1);
+        x(i+1) = temp;
+    end
+end 
+y=Sort_array(x(1:n-1));
+y = [y x(n)];
+end
